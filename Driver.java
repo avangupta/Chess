10576a0cc67000f70d92174c6b6520376998c7d9
Avan Gupta <96600087+avangupta@users.noreply.github.com>
2026-01-06 20:24:10 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -6,7 +6,7 @@ public class Driver  {
     public static void main(String[] args)
     {
         JFrame frame = new JFrame("Chess");
-        frame.setSize(ChessPanel.getFrame() + 1, ChessPanel.getFrame() + 1);
+        frame.setSize(ChessPanel.getHFrame() + 1, ChessPanel.getVFrame() + 1);
         frame.setLocation(0, 0);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(new ChessPanel());
